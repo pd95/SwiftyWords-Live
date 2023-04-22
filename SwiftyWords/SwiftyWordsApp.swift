67b56e5deb8a48332136f69b84d3123a38b22d6d
Philipp <pd95@users.noreply.github.com>
2023-04-22 18:12:33 +0200

    Initial Commit

---
+++ SwiftyWords/SwiftyWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWordsApp.swift
+//  SwiftyWords
+//
+//  Created by Philipp on 22.04.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
